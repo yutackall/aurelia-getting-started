f21058122955d81c6d5725383d3ca4f09dadf361
yutackall <yutackall@gmail.com>
2016-06-23 00:45:51 +0900

    Adding a Second Page

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ export class App {
     config.title = 'Aurelia';
     config.map([
       { route: ['', 'welcome'], name: 'welcome', moduleId: './welcome', nav: true, title: 'Welcome' },
+      { route: 'users',         name: 'users',   moduleId: './users',   nav: true, title: 'Github Users'}
     ]);
 
     this.router = router;
